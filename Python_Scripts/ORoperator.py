81d34973dfc7a0dd1bf3666069f75f020751bd5c
LavanyaYetham <61427350+LavanyaYetham@users.noreply.github.com>
2024-05-07 09:36:24 +0530

    Update ORoperator.py

--- Python_Scripts/ORoperator.py
+++ Python_Scripts/ORoperator.py
@@ -1,7 +1,7 @@
 
 #!/usr/local/bin/python3.8
 gen=input("Are you a Male(y/n): ")
-if gen=='y' or gen=="OR":
+if gen=='y' or gen=="Y":
     print ("Gender: Male")
 else:
     print ("Gender: Female")
